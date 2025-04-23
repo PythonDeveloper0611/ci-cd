2bc44be50c2a8916d1425c1bda00e96bcdd23e33
mithunchandra23 <mithun.chilukamary2309@gmail.com>
2025-04-23 17:27:48 +0530

    Initial commit

---
+++ application.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello from AWS Elastic Beanstalk with CI/CD!"
+
+if __name__ == "__main__":
+    app.run()
